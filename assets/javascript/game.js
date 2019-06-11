e28affe8d6d7b003eaf31166e32743372eab80f6
sharpevb <victoriabsharpe@gmail.com>
2019-06-10 23:52:44 -0400

    bok choy is a go

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,76 @@
+var restart = false    
+var totalMin = 19
+var totalMax = 120
+var cabMin = 1
+var cabMax = 12
+var bokVal = 1 //ex
+var redVal
+var napaVal
+var usaVal
+var currentNum = 0 //ex
+var wins = 0
+var loses = 0
+
 $(document).ready(function() {
-
-var min = 19
-var max = 120
-
-//Each cabbage generating a different value between 1-12
-$("#bok").on("click", function() {
-    var min = 1
-    var max = 12
-var random = Math.floor(Math.random() * (max - min +1 )) + min;
-alert(random);
-})
-
-$("#red").on("click", function() {
-    var min = 1
-    var max = 12
-var random = Math.floor(Math.random() * (max - min +1 )) + min;
-alert(random);
-})
-
-$("#napa").on("click", function() {
-    var min = 1
-    var max = 12
-var random = Math.floor(Math.random() * (max - min +1 )) + min;
-alert(random);
-})
-
-$("#usa").on("click", function() {
-    var min = 1
-    var max = 12
-var random = Math.floor(Math.random() * (max - min +1 )) + min;
-alert(random);
-})
+ 
 
 
 
+// Target Number 
+if (randomTotal = Math.floor(Math.random() * (totalMax - totalMin + 1 )) + totalMin) {
+   $("#randomNumber").html(randomTotal) 
+};
 
+// Setting current number to 0
+if (currentNum >= 0) {
+    $("#total").html(currentNum) 
+ };
 
+// Wins
+ if (wins >= 0) {
+     $("#wins").html(wins)
+ }
 
-/*function getRandomInt(min, max) {
-if () {
-
-}
-    var min = 19
-    var max = 120
-    
+// Loses
+ if (loses >= 0) {
+    $("#loses").html(loses)
 }
 
-console.log(this);
+
+
+
+
+/*Each cabbage generating a different value between 1-12
+if (cabbageValue = Math.floor(Math.random() * (cabMax - cabMin + 1 )) + cabMin) {
+    $("#bok").attr(cabbageValue);
+    console.log(cabbageValue);
+}
+*/
+
+
+//if randomTotal >= cabbageValue {
+
+//}
+
+
+// Add all of the cabbage values together
+
+
+ //Example cab random value
+ 
+
+/* if (bokVal = Math.floor(Math.random() * (cabMax - cabMin +1 )) + cabMin) {
+
+}
+ $("#bok").on("click", function() {
+
+;
+
+})
 
 */
 
-// Target Number 
-if (randomTotal = Math.floor(Math.random() * (max - min +1 )) + min) {
-   $("#randomNumber").html(randomTotal) 
-};
+
 
 
 
@@ -64,4 +81,10 @@ if (randomTotal = Math.floor(Math.random() * (max - min +1 )) + min) {
 
 });
 
-
+// This works, don't fuck it up k thx
+function addBok() {
+currentNum = currentNum + bokVal
+$("#total").html(currentNum) 
+console.log(bokVal);
+console.log(currentNum);
+} 
