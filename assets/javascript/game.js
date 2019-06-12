8813173d1dc34579ce79272db1d0fe57c92928e4
sharpevb <victoriabsharpe@gmail.com>
2019-06-11 20:40:41 -0400

    pseudo coding

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,74 +1,76 @@
-var restart = false    
+var restart = false
 var totalMin = 19
 var totalMax = 120
 var cabMin = 1
 var cabMax = 12
 var bokVal = 1 //ex
-var redVal
-var napaVal
-var usaVal
+var redVal = 2 //ex
+var napaVal = 3 //ex
+var usaVal = 4 //ex
 var currentNum = 0 //ex
 var wins = 0
 var loses = 0
+var randomTotal
 
-$(document).ready(function() {
- 
+$(document).ready(function () {
+
 
 
 
 // Target Number 
-if (randomTotal = Math.floor(Math.random() * (totalMax - totalMin + 1 )) + totalMin) {
-   $("#randomNumber").html(randomTotal) 
+if (randomTotal = Math.floor(Math.random() * (totalMax - totalMin + 1)) + totalMin) {
+$("#randomNumber").html(randomTotal);
 };
 
 // Setting current number to 0
 if (currentNum >= 0) {
-    $("#total").html(currentNum) 
- };
+$("#total").html(currentNum);
+};
 
 // Wins
- if (wins >= 0) {
-     $("#wins").html(wins)
- }
+if (wins >= 0) {
+$("#wins").html(wins);
+wins++;
+};
 
 // Loses
- if (loses >= 0) {
-    $("#loses").html(loses)
-}
+if (loses >= 0) {
+$("#loses").html(loses);
+};
 
 
 
 
 
-/*Each cabbage generating a different value between 1-12
-if (cabbageValue = Math.floor(Math.random() * (cabMax - cabMin + 1 )) + cabMin) {
-    $("#bok").attr(cabbageValue);
-    console.log(cabbageValue);
-}
-*/
+    /*Each cabbage generating a different value between 1-12
+    if (cabbageValue = Math.floor(Math.random() * (cabMax - cabMin + 1 )) + cabMin) {
+        $("#bok").attr(cabbageValue);
+        console.log(cabbageValue);
+    }
+    */
 
 
-//if randomTotal >= cabbageValue {
+    //if randomTotal >= cabbageValue {
 
-//}
-
-
-// Add all of the cabbage values together
+    //}
 
 
- //Example cab random value
- 
+    // Add all of the cabbage values together
 
-/* if (bokVal = Math.floor(Math.random() * (cabMax - cabMin +1 )) + cabMin) {
 
-}
- $("#bok").on("click", function() {
+    //Example cab random value
 
-;
 
-})
-
-*/
+    /* if (bokVal = Math.floor(Math.random() * (cabMax - cabMin +1 )) + cabMin) {
+    
+    }
+     $("#bok").on("click", function() {
+    
+    ;
+    
+    })
+    
+    */
 
 
 
@@ -81,10 +83,34 @@ if (cabbageValue = Math.floor(Math.random() * (cabMax - cabMin + 1 )) + cabMin) 
 
 });
 
-// This works, don't fuck it up k thx
+// BOK
 function addBok() {
-currentNum = currentNum + bokVal
-$("#total").html(currentNum) 
-console.log(bokVal);
-console.log(currentNum);
+    currentNum = currentNum + bokVal
+    $("#total").html(currentNum)
+    console.log(bokVal);
+    console.log(currentNum);
+}
+
+// RED
+function addRed() {
+    currentNum = currentNum + redVal
+    $("#total").html(currentNum)
+    console.log(redVal);
+    console.log(currentNum);
+}
+
+// NAPA
+function addNapa() {
+    currentNum = currentNum + napaVal
+    $("#total").html(currentNum)
+    console.log(napaVal);
+    console.log(currentNum);
+}
+
+// USA
+function addUsa() {
+    currentNum = currentNum + usaVal
+    $("#total").html(currentNum)
+    console.log(usaVal);
+    console.log(currentNum);
 } 
