02dd5dc4427afbad76132596b342f68aa015f791
sharpevb <victoriabsharpe@gmail.com>
2019-06-14 23:55:42 -0400

    trying to get bok the same value

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,11 +3,11 @@ var totalMin = 19
 var totalMax = 120
 var cabMin = 1
 var cabMax = 12
-var bokVal = 1 //ex
+var cabArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 var redVal = 2 //ex
 var napaVal = 3 //ex
 var usaVal = 4 //ex
-var currentNum = 0 //ex
+var currentNum = 0
 var wins = 0
 var loses = 0
 var randomTotal
@@ -15,80 +15,42 @@ var randomTotal
 $(document).ready(function () {
 
 
+    // Target Number 
+    if (randomTotal = Math.floor(Math.random() * (totalMax - totalMin + 1)) + totalMin) {
+        $("#randomNumber").html(randomTotal);
+    };
 
+    // Setting current number to 0
+    if (currentNum >= 0) {
+        $("#total").html(currentNum);
+    };
 
-// Target Number 
-if (randomTotal = Math.floor(Math.random() * (totalMax - totalMin + 1)) + totalMin) {
-$("#randomNumber").html(randomTotal);
-};
-
-// Setting current number to 0
-if (currentNum >= 0) {
-$("#total").html(currentNum);
-};
-
-// Wins
-if (wins >= 0) {
-$("#wins").html(wins);
-wins++;
-};
-
-// Loses
-if (loses >= 0) {
-$("#loses").html(loses);
-};
-
-
-
-
-
-    /*Each cabbage generating a different value between 1-12
-    if (cabbageValue = Math.floor(Math.random() * (cabMax - cabMin + 1 )) + cabMin) {
-        $("#bok").attr(cabbageValue);
-        console.log(cabbageValue);
+    // Wins
+    if (currentNum === randomTotal) {
+        $("#wins").html(wins);
+        wins++;
     }
-    */
-
-
-    //if randomTotal >= cabbageValue {
-
-    //}
-
-
-    // Add all of the cabbage values together
-
-
-    //Example cab random value
-
-
-    /* if (bokVal = Math.floor(Math.random() * (cabMax - cabMin +1 )) + cabMin) {
-    
+    // Loses
+    else if (currentNum >= randomTotal) {
+        $("#loses").html(loses);
+        loses++;
     }
-     $("#bok").on("click", function() {
-    
-    ;
-    
-    })
-    
-    */
-
-
-
-
-
-
-
-
 
 
 });
 
 // BOK
 function addBok() {
-    currentNum = currentNum + bokVal
+    for (var i = 0; i < cabArray.length; i++) {
+        var bokVal = cabArray[Math.floor(Math.random() * cabArray.length)];
+        bokVal === cabArray[i];
+    }
+    
+    bokVal = parseInt(bokVal);
+    currentNum += bokVal
     $("#total").html(currentNum)
+
     console.log(bokVal);
-    console.log(currentNum);
 }
 
 // RED
@@ -114,3 +76,6 @@ function addUsa() {
     console.log(usaVal);
     console.log(currentNum);
 } 
+
+
+
